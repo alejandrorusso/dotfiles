2ee1fe1995c72424803d4dc2d2c56fdb68069cdd
Your name here <you@email.address>
2026-04-25 19:15:19 +0000

    random ansi 30% of the time

--- bash/bashrc.d/80-ansi-welcome.sh
+++ bash/bashrc.d/80-ansi-welcome.sh
@@ -1,4 +1,4 @@
-# Random ANSI welcome banner (only if the repo has been cloned).
-if [ -d "$HOME/.ansi-welcome" ] && [ -x "$HOME/.ansi-welcome/random-ansi.sh" ]; then
+# Random ANSI welcome banner (only if the repo has been cloned, 30% of the time).
+if [ -d "$HOME/.ansi-welcome" ] && [ -x "$HOME/.ansi-welcome/random-ansi.sh" ] && [ $((RANDOM % 100)) -lt 30 ]; then
   ( cd "$HOME/.ansi-welcome" && ./random-ansi.sh --cache )
 fi
